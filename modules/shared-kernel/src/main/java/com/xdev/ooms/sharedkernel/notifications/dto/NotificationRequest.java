ae8fc941063da341a8d5e72bc6e8947122d32535
ismail.mansouri <ismail.mansouri@rhis-solutions.com>
2026-06-20 19:46:54 +0100

    railway build

--- modules/shared-kernel/src/main/java/com/xdev/ooms/sharedkernel/notifications/dto/NotificationRequest.java
+++ modules/shared-kernel/src/main/java/com/xdev/ooms/sharedkernel/notifications/dto/NotificationRequest.java
@@ -1,18 +1,54 @@
 package com.xdev.ooms.sharedkernel.notifications.dto;
 
-import lombok.*;
-
 import java.util.List;
 import java.util.Map;
 
-@Data
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class NotificationRequest {
+
     private List<String> userIds;
     private String title;
     private String message;
-    private Map<String, String> data;   // pour navigation mobile
+    private Map<String, String> data;
+
+    public NotificationRequest() {
+    }
+
+    public NotificationRequest(List<String> userIds, String title, String message, Map<String, String> data) {
+        this.userIds = userIds;
+        this.title = title;
+        this.message = message;
+        this.data = data;
+    }
+
+    public List<String> getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(List<String> userIds) {
+        this.userIds = userIds;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, String> data) {
+        this.data = data;
+    }
 }
